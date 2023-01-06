429fbc8abbc814fa59cc65e44669ea69d20b6253
Leesumin <leesumin808@gmail.com>
2023-01-06 09:50:35 +0900

    23/01/06 백준 - 2902

---
+++ coding/src/coding/Baek_2902.java
@@ -0,0 +1,28 @@
+package coding;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+
+public class Baek_2902 {
+
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		String[] txt = br.readLine().split("-");
+		
+		StringBuilder sb = new StringBuilder();
+		
+		for(int i = 0; i < txt.length; i++) {
+			
+			if(txt[i].charAt(0) != '-') {
+				sb.append(txt[i].charAt(0));
+			}
+		}
+
+		System.out.println(sb);
+	}
+
+}
