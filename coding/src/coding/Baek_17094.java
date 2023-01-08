d73f5ba203323e275b73413cc12b2dd744333a8c
Leesumin <leesumin808@gmail.com>
2023-01-08 12:35:52 +0900

    23/01/08 - 백준 - 17094

---
+++ coding/src/coding/Baek_17094.java
@@ -0,0 +1,30 @@
+package coding;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+
+public class Baek_17094 {
+
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int num = Integer.parseInt(br.readLine());
+		
+		String txt = br.readLine().substring(0,num);
+		
+		String txt2 = txt.replaceAll("e", "");
+		String txtE = txt.replaceAll("2", "");
+		  
+		if(txt2.toCharArray().length > txtE.toCharArray().length) {
+			System.out.println("2");
+		} else if(txt2.toCharArray().length < txtE.toCharArray().length){ 
+			System.out.println("e"); 
+		} else {
+		  System.out.println("yee"); 
+		}
+	}
+
+}
