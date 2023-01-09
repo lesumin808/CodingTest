2fe2804693d6a6acb38e7fc4f5586f4399f5440b
Leesumin <leesumin808@gmail.com>
2023-01-09 20:46:51 +0900

    23/01/09 백준 - 7567

---
+++ coding/src/coding/Baek_7567.java
@@ -0,0 +1,32 @@
+package coding;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+
+public class Baek_7567 {
+
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		String dish = br.readLine();
+		
+		int height = 0;
+		
+		for(int i = 1; i < dish.length(); i++) {
+			if(dish.charAt(i-1) == dish.charAt(i)) {
+				height += 5;
+			}else {
+				height += 10;
+			}
+		}
+		
+		height += 10;
+		
+		System.out.println(height);
+
+	}
+
+}
