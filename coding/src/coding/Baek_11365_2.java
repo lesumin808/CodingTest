51569656c3d3645b108928dbb386f148d263336e
Leesumin <leesumin808@gmail.com>
2023-01-16 17:36:55 +0900

    23/01/16 백준 - 11365

---
+++ coding/src/coding/Baek_11365_2.java
@@ -0,0 +1,32 @@
+package coding;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+
+public class Baek_11365_2 {
+
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		String txt = "";
+		String text = "";
+		
+		while(!"END".equals(txt = br.readLine())){
+			
+			for(int i = txt.length() - 1; i >= 0; i--) {
+				text += txt.charAt(i);
+			}
+			
+			text += "\n";
+		}
+		
+		System.out.println(text);
+		
+		
+		
+	}
+
+}
